629f2788d45c946b203e1cf3582ec005601dddac
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-08 16:22:35 +0530

    Auto push

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "public_subnet" {
-  count           = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id          = aws_vpc.main
-  cidr_block      = element(var.PUBLIC_SUBNET_CIDR, count.index) 
+  count                = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id               = aws_vpc.main.id
+  cidr_block           = element(var.PUBLIC_SUBNET_CIDR, count.index) 
+  availability_zone_id = element(var.AZ, count.index)
 
   tags = {
-    Name  = "roboshop-${var.ENV}-public-subnet"
+    Name  = "roboshop-${var.ENV}-pub-subnet-${element(var.AZ, count.index)}"
   }
 }
