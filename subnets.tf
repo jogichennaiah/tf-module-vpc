cffd8111d52a90bf162cfd56be15951115514cbd
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-08 16:16:04 +0530

    Auto push

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
   count           = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id          = aws_vpc.main.id
+  vpc_id          = aws_vpc.main
   cidr_block      = element(var.PUBLIC_SUBNET_CIDR, count.index) 
 
   tags = {
