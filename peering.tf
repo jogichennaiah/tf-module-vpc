212e39be943a629ab1b77b34a87e7a476197f747
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-09 23:59:47 +0530

    Auto push

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 # Creates peering between Default and project VPC
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer" {
   vpc_id        = var.DEFAULT_VPC_ID
   peer_vpc_id   = aws_vpc.main.id
   auto_accept   = true                                    # This is valid if both vpc's are in the same account.
