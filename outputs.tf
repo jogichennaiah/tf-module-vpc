39887ae223230cb5dbdff8476b2283f9348a70f1
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-31 22:55:58 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,31 @@
-output "vpc_ID" {
-    value = aws_vpc.main.id
+output "VPC_ID" {
+    value = aws_vpc.main.id 
 }
 
-output "vpc_CIDR" {
-  value  =  aws_vpc.main.cidr_block
+output "VPC_CIDR" {
+    value = aws_vpc.main.cidr_block
 }
 
 output "PUBLIC_SUBNET_CIDR" {
-  value  =  aws_subnet.public_subnet.*.cidr_block
+    value = aws_subnet.public_subnet.*.cidr_block 
 }
 
 output "PRIVATE_SUBNET_CIDR" {
-  value  =  aws_subnet.private_subnet.*.cidr_block
+    value = aws_subnet.private_subnet.*.cidr_block
 }
 
 output "PUBLIC_SUBNET_IDS" {
-  value  =  aws_subnet.public_subnet.*.id
+    value = aws_subnet.public_subnet.*.id 
 }
 
 output "PRIVATE_SUBNET_IDS" {
-  value  =  aws_subnet.private_subnet.*.id
+    value = aws_subnet.private_subnet.*.id
 }
 
 output "DEFAULT_VPC_ID" {
-  value  =  var.DEFAULT_VPC_ID
+    value = var.DEFAULT_VPC_ID
 }
 
 output "DEFAULT_VPC_CIDR" {
-  value  =  var.DEFAULT_VPC_CIDR
+    value = var.DEFAULT_VPC_CIDR
 }
-
-
-
-
-
